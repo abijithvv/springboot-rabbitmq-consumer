c17bcf1253a5ffdb0c5ddb52b272411029be6d92
Abijith Viswanath <abijithvv@gmail.com>
2023-11-14 16:07:35 -0500

    First commit of rabbit MQ consumer app code using spring-boot

---
+++ src/main/java/co/abi/learning/springrabbitconsumer/SpringRabbitConsumerApplication.java
@@ -0,0 +1,13 @@
+package co.abi.learning.springrabbitconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRabbitConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRabbitConsumerApplication.class, args);
+    }
+
+}
